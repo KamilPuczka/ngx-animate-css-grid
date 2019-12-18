d3df6540da937fe4b2bc713066915dfcc159185a
Kamil Puczka <kamil.puczka@iteo.com>
2019-12-18 19:59:29 +0100

    INIT: Library

---
+++ projects/ng-animate-css-grid/src/lib/ng-animate-css-grid.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NgAnimateCssGridDirective } from './ng-animate-css-grid.directive';
+
+describe('NgAnimateCssGridDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NgAnimateCssGridDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/ng-animate-css-grid/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-animate-css-grid
+ */
+
+export * from './lib/ng-animate-css-grid.module';
+export * from './lib/ng-animate-css-grid.directive';
+export * from './lib/wrap-grid-config';
